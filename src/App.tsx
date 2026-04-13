8ae888ab5d02b751f92b43ae111ae08aa98af49c
Sathiyan <sathiyans2003@github.com>
2026-04-13 18:50:32 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import ThankYou from "./pages/ThankYou";
 import NotFound from "./pages/NotFound";
 
+
 const queryClient = new QueryClient();
 
 const App = () => (
